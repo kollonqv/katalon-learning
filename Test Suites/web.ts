ac7dba8e1e4277b62026d2e679e410660b88dd3c
Kalle <ollonqvistkalle@gmail.com>
2023-11-06 11:36:29 -0800

    - Add Sign In test case and objects (#2)

---
+++ Test Suites/web.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>web</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f84ebb6d-7ebf-4ae6-998e-b289a35a755c</testSuiteGuid>
+   <testCaseLink>
+      <guid>fddf7837-8ed1-4752-9244-75afde08f063</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI/Sign In</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
